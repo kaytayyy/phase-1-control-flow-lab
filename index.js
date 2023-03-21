c14f94047de1842e3dedb0d24d139df3f4b39403
Inda Smith <inda.k.smith@gmail.com>
2023-03-20 20:48:20 -0400

    first test is passing, the rest are not

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let result
+  if (feet <= 400) {
+    return ("This one is on me!");
+  }
+  else if (2000 > feet > 400) {
+    return ("That will be twenty bucks.");
+  }
+return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city ? NYC : !NYC);
 }
+console.log(ternaryCheckCity(true));
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
